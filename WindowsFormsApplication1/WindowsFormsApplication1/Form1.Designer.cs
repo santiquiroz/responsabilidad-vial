ccec3cff55fe369b8e4c173a9cf0f315338e2f06
Santiago Quiroz upegui <squirozu@unal.edu.co>
2019-12-05 21:32:58 -0500

    forma funcional del minijuego
    faltan añadir pantalla de inicio y pantalla de perdida

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.carro1 = new System.Windows.Forms.PictureBox();
+            this.lblPuntos = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.carro1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // carro1
+            // 
+            this.carro1.BackColor = System.Drawing.Color.Transparent;
+            this.carro1.Image = global::WindowsFormsApplication1.Properties.Resources.carritouno_;
+            this.carro1.Location = new System.Drawing.Point(3, 414);
+            this.carro1.Margin = new System.Windows.Forms.Padding(4);
+            this.carro1.Name = "carro1";
+            this.carro1.Size = new System.Drawing.Size(45, 90);
+            this.carro1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.carro1.TabIndex = 0;
+            this.carro1.TabStop = false;
+            // 
+            // lblPuntos
+            // 
+            this.lblPuntos.AutoSize = true;
+            this.lblPuntos.BackColor = System.Drawing.Color.Transparent;
+            this.lblPuntos.Font = new System.Drawing.Font("Elephant", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPuntos.ForeColor = System.Drawing.Color.White;
+            this.lblPuntos.Location = new System.Drawing.Point(16, 11);
+            this.lblPuntos.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPuntos.Name = "lblPuntos";
+            this.lblPuntos.Size = new System.Drawing.Size(19, 18);
+            this.lblPuntos.TabIndex = 1;
+            this.lblPuntos.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.carretera;
+            this.ClientSize = new System.Drawing.Size(197, 517);
+            this.Controls.Add(this.lblPuntos);
+            this.Controls.Add(this.carro1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.carro1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox carro1;
+        private System.Windows.Forms.Label lblPuntos;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        List<PictureBox> listaObstaculosAmarillo = new List<PictureBox>();
+        Random RnTipoObstaculo = new Random();
+        int Velocidad = 3;
+        int AnimacionCarro1;
+        public Form1()
+        {
+            //Inicializando los componentes del juego y por lo tanto el juego en si
+            InitializeComponent();
+            /*SoundPlayer simpleSound = new SoundPlayer("./Resources/back_in_black_8_bits.wav");
+            simpleSound.Play();*/
+
+            //Reproduciendo musica de fondo
+
+            System.Media.SoundPlayer logout = new System.Media.SoundPlayer(Properties.Resources.back_in_black_8_bits);
+            logout.Play();
+            
+            CrearObstaculo(listaObstaculosAmarillo, this, 10, 80);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            carro1.Image = (AnimacionCarro1 == 0) ? Properties.Resources.carritouno : Properties.Resources.carritouno_;
+            AnimacionCarro1 = (AnimacionCarro1 == 0) ? 1 : 0;
+        }
+
+        public void CrearObstaculo(List<PictureBox> ListaElementos, Form panelJuegoUno, int distanciaUno, int distanciaDos)
+        {
+            int NumeroCarro = 1;
+            int TipoObstaculo = RnTipoObstaculo.Next(1, 3);
+
+
+
+            int UbicacionObstaculo = RnTipoObstaculo.Next(1, 3);
+
+            int DIstanciaUbicaciónObstaculo = (UbicacionObstaculo == 1) ? distanciaUno : distanciaDos;
+
+            PictureBox pb = new PictureBox();
+            pb.Location = new Point(DIstanciaUbicaciónObstaculo, 0);
+
+
+            pb.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("obstaculo" + TipoObstaculo);
+
+            pb.BackColor = Color.Transparent;
+            pb.Tag = NumeroCarro + "_" + TipoObstaculo;
+            pb.SizeMode = PictureBoxSizeMode.AutoSize;
+            ListaElementos.Add(pb);
+            panelJuegoUno.Controls.Add(pb);
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            foreach (PictureBox ImagenCarro in listaObstaculosAmarillo)
+            {
+                int MovimientoY;
+                MovimientoY = ImagenCarro.Location.Y;
+                MovimientoY = MovimientoY + Velocidad;
+                ImagenCarro.Location = new Point(ImagenCarro.Location.X, MovimientoY);
+            }
+
+            if (listaObstaculosAmarillo.Count > 0)
+            {
+                if (listaObstaculosAmarillo[(listaObstaculosAmarillo.Count) - 1].Location.Y > 250)
+                {
+                    CrearObstaculo(listaObstaculosAmarillo, this, 10, 80);
+                }
+            }
+            if (listaObstaculosAmarillo.Count > 0)
+
+            {
+                for (int i = 0; i < listaObstaculosAmarillo.Count; i++)
+                {
+                    if (listaObstaculosAmarillo[i].Location.Y > 400)
+                    {
+                        if (listaObstaculosAmarillo[i].Tag.ToString() == "1_1")
+                        {
+                            ReiniciarJuego();
+                        }
+                        this.Controls.Remove(listaObstaculosAmarillo[i]);
+                        listaObstaculosAmarillo.Remove(listaObstaculosAmarillo[i]);
+                    }
+                    if (listaObstaculosAmarillo[i].Bounds.IntersectsWith(carro1.Bounds))
+                    {
+                        if (listaObstaculosAmarillo[i].Tag.ToString() == "1_1")
+                        {
+                            this.Controls.Remove(listaObstaculosAmarillo[i]);
+                            int TotalPuntos = Convert.ToInt32(lblPuntos.Text) + 1;
+                            if (TotalPuntos % 2 == 0)
+                            {
+                                Velocidad++;
+                            }
+                            lblPuntos.Text = TotalPuntos.ToString();
+
+                            listaObstaculosAmarillo.Remove(listaObstaculosAmarillo[i]);
+                        }
+                        else
+                        {
+                            this.Controls.Remove(listaObstaculosAmarillo[i]);
+                            listaObstaculosAmarillo.Remove(listaObstaculosAmarillo[i]);
+                            ReiniciarJuego();
+
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int CambioCoches = (carro1.Location.X == 80) ? 10 : 80;
+            carro1.Location = new Point(CambioCoches, carro1.Location.Y);
+        }
+        public void ReiniciarJuego()
+        {
+            lblPuntos.Text = "0";
+            Velocidad = 3;
+        }
+    }
+}
